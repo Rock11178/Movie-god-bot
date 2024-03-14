97d9c96c9c2f1875f3d86f017b497197b9af86aa
Shiva7774 <156524764+Shiva7774@users.noreply.github.com>
2024-03-14 07:52:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://shivamcharan7773:shivacharan7@cluster0.se91yp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #mongo db url
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://shivacharan7773:shiva77@cluster0.wnzsz3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #mongo db url
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
