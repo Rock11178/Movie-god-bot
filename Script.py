c9d582b48d8fe0204db7aeff611a926c0bcc3ac3
Shiva7774 <156524764+Shiva7774@users.noreply.github.com>
2024-03-30 07:11:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
 ɪ ᴀᴍ ᴘᴏᴡᴇʀ ғᴜʟʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ
 ɪ ᴀᴍ ᴘᴍ sʜᴏʀᴛɴᴇʀ-ʟɪɴᴋ ʙᴏᴛ
 
-ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/shivacharan7773">𝙎𝙃𝙄𝙑𝘼</a></b> 🤠</b>"""
+ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/shivacharan7773">𝙎𝙃𝙄𝙑𝘼</a></b> 😎</b>"""
 
     
     HELP_TXT = """<b>Hᴇʏ {}
